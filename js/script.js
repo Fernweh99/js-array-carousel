e7ea5d860c9bc5a118ed351afb9a105c3b0ecef0
Fernweh99 <daniele.dimento@gmail.com>
2022-06-27 15:55:22 +0200

    finish mileston 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 /*
-MILESTONE 1 :
-Per prima cosa, creiamo il markup statico: costruiamo il container
-e inseriamo l'immagine grande in modo da poter stilare lo slider;
-avremo così la struttura base e gli stili pronti per poterci
-poi concentrare solamente sull'aspetto logico.
+MILESTONE 2 :
+Adesso rimuoviamo tutto il markup statico e inseriamo
+le immagini dinamicamente servendoci dell'array fornito 
+e un semplice ciclo for. Possiamo concatenare una stringa
+con un template literal oppure utilizzare gli altri metodi
+di manipolazione del DOM che abbiamo visto insieme.
+Al termine di questa fase ci ritroveremo con lo stesso
+slider, ma costruito dinamicamente attraverso JavaScript.
 */
+
+// Richiamo la classe gallery dal DOM
+const gallery = document.querySelector(".gallery-image");
+
+// Creo l'array
+const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+
+//creo una variabile per concatenare le immagini e stamparle su pagina
+let galleryImages = "";
+
+//Eseguo un for per concatenare e creare la stinga con tutte le immagini
+for (let i = 0; i < images.length; i++) {
+  galleryImages += `<img src="img/${images[i]}" alt="image${i + 1}" />`;
+
+}
+
+//stampo su pagina la stringa pronta con le immagini
+gallery.innerHTML = galleryImages;
